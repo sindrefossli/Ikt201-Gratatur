d1ae56c25f4d7721445d1ac30b5c9d483b55ca5b
sindrefossli <Sindref@uia.no>
2024-10-28 21:07:43 +0100

    tok away the api

---
+++ NorMall/Services/EmailSender.cs
@@ -0,0 +1,24 @@
+﻿namespace NorMall.Services;
+
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+
+
+internal sealed class EmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+
+        var apiKey = "";
+        var client = new SendGridClient(apiKey);
+        var from = new EmailAddress("yoho1423@gmail.com", "NorMall");
+        var to = new EmailAddress(email, "Customer");
+        var plainTextContent = "Confirm email address";
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlMessage);
+
+        var response = client.SendEmailAsync(msg);
+        
+        return response;
+    }
+}
